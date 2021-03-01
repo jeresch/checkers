9f1a58dda08e58336a3694bcfc781a9b5a640061
Jeremy Schroeder <jpschroeder2014@gmail.com>
2021-03-01 16:31:53 -0500

    Bootstrap webpack for client

---
+++ client/src/constants.js
@@ -0,0 +1,4 @@
+export const constants = {
+    hostname: "localhost",
+    port: "8080",
+};
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import { constants } from "./constants";
+
+document.getElementById("create-game-button").addEventListener("click", (elem, event) => {
+    document.body.innerHTML = "Hello";
+});
