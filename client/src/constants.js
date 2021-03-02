8d0385139cb709ef50200f4fc06202898e69c64e
Jeremy Schroeder <jpschroeder2014@gmail.com>
2021-03-02 09:20:47 -0500

    Add board drawing function

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,7 @@
 export const constants = {
     hostname: "localhost",
     port: "8080",
+    boardWidth: 200,
+    boardHeight: 200,
+    boardDimension: 8,
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,19 @@
 import { constants } from "./constants";
 
-document.getElementById("create-game-button").addEventListener("click", (elem, event) => {
-    document.body.innerHTML = "Hello";
-});
+const drawBoard = (ctx, state) => {
+    const tileWidth = constants.boardWidth / constants.boardDimension;
+    const tileHeight = constants.boardHeight / constants.boardDimension;
+    for (let row = 0; row < constants.boardDimension; row++) {
+        for (let col = 0; col < constants.boardDimension; col++) {
+            ctx.fillStyle = ((row + col) % 2 == 0) ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)";
+            ctx.fillRect(col * tileWidth, row * tileHeight, tileWidth, tileHeight);
+            console.log(col * tileWidth, row * tileHeight);
+        }
+    }
+};
+
+let canvas = document.getElementById("game-board");
+canvas.setAttribute("width", `${constants.boardWidth}`);
+canvas.setAttribute("height", `${constants.boardHeight}`);
+let ctx = canvas.getContext("2d");
+drawBoard(ctx);
