c75539e734be730024946520f40c4a8b2dc5560c
Metty Schroeder <mettyj.schroeder@gmail.com>
2021-03-02 19:06:55 -0500

    Initial server (#10)
    * Grpc version update
    
    * Intial server structure
    
    * Update to generated proto imports

--- server/main.go
+++ server/main.go
@@ -2,5 +2,48 @@ package main
 
 //go:generate bash -c "protoc --experimental_allow_proto3_optional --proto_path=../protobuf --go_out=generated/ --go-grpc_out=generated/ $(find ../protobuf -type f -name *.proto)"
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	gppb "github.com/MettyS/checkers/server/generated"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+}
+
+func (s *server) MakeMoves(ctx context.Context, req *gppb.MoveRequest) *gppb.MoveResponse {
+
+	fmt.Println("MakeMoves Running!")
+
+	res := &gppb.MoveResponse{
+		MoveSuccess: true,
+	}
+
+	return res
+}
+
+// BoardUpdateSubscription
+
+// rpc MakeMoves(MoveRequest) returns (MoveResponse) {}
+// rpc BoardUpdateSubscription(BoardSubscriptionRequest) returns (stream BoardUpdate) {}
+
 func main() {
+	lis, err := net.Listen("tcp", ":9000")
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	checkersServer := grpc.NewServer()
+
+	s := server{}
+	gppb.RegisterGameplayServiceServer(checkersServer, &s)
+
+	if err := checkersServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 }
